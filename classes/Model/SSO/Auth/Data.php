b5a2629ba36b3521bdb8bbc5acaed640761ce182
biakaveron <biakaveron@gmail.com>
2013-01-15 09:48:04 +0400

    Added get_avatar() method

--- classes/Model/SSO/Auth/Data.php
+++ classes/Model/SSO/Auth/Data.php
@@ -4,8 +4,50 @@ class Model_SSO_Auth_Data extends ORM {
 
 	protected $_table_name = 'auth_data';
 
-	protected $_belongs_to = array(
-		'user' => array(),
-	);
+	/**
+	 * Get avatar URL
+	 *
+	 * @param  int $size  used for gravatar images only
+	 *
+	 * @return mixed|null|string
+	 */
+	public function get_avatar($size = NULL)
+	{
+		$avatar = $this->avatar;
+		if (empty($avatar) AND ! empty($this->email) )
+		{
+			// use email as Gravatar ID
+			$avatar = md5($this->email);
+		}
+
+		if (empty($avatar))
+		{
+			return NULL;
+		}
+
+		if (strpos($avatar, '://') == FALSE)
+		{
+			// its a Gravatar ID
+			$avatar = 'http://gravatar.com/avatar/' . $avatar;
+			$params = array();
+			if (empty($avatar))
+			{
+				// use default Gravatar
+				$params['f'] = 'y';
+			}
+
+			if ($size)
+			{
+				$params['s'] = intval($size);
+			}
+
+			if ( ! empty($params) )
+			{
+				$avatar .= http_build_query($params);
+			}
+		}
+
+		return $avatar;
+	}
 
 }
